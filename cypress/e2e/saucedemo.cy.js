5ec62f7416a02756fb5e6ba4e1c200bd8898246b
fabiana.silva <fabiana.silva@eemovel.com>
2025-06-16 09:18:28 -0300

    fix tests

--- cypress/e2e/saucedemo.cy.js
+++ cypress/e2e/saucedemo.cy.js
@@ -1,5 +1,5 @@
 describe('Google Search', () => {
-  it('Should search for Cypress on Google', () => {
+  it('Login in saucedemo', () => {
     cy.visit('https://www.saucedemo.com/');
     cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="password"]').type('secret_sauce');
