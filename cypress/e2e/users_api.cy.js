73e8141911cfd90c14421a0d5d75dd1953223f25
fabiana.silva <fabiana.silva@eemovel.com>
2025-06-16 21:12:44 -0300

    fix tests

--- cypress/e2e/users_api.cy.js
+++
@@ -1,9 +0,0 @@
-describe('API Test - Users', () => {
-  it('Should get a list of users', () => {
-    cy.request('https://reqres.in/api/users?page=2')
-      .should((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('data');
-      });
-  });
-});
